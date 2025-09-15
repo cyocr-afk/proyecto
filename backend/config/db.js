d4aed6fee3e445bc881c547e79c12a0a82aca13b
cyocr-afk <cyocr@miumg.edu.gt>
2025-09-14 22:02:25 -0600

    fix: agregar ssl

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,10 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+  database: process.env.DB_NAME,
+ssl: { rejectUnauthorized: false }
+  
+}); 
 
 connection.connect((err) => {
   if (err) {
